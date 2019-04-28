b49b8763112e8a1434c8c8b0a31166a3d85c9256
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2019-04-28 17:31:59 +0200

    docker: always remove container even when dropping into a shell

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if ! grep -c docker /proc/1/cgroup > /dev/null; then
     image_tag="$(basename $PWD | sed 's/_/-/g' | cut -c 1-40 | tr '[:upper:]' '[:lower:]'):$(git rev-parse --short HEAD)"
     docker build --build-arg BUILD_UID=$(id -u) --build-arg BUILD_GID=$(id -g) --tag local/$image_tag .
     if [[ $1 == "--shell" ]]; then
-        docker run --name $(basename $PWD) -it -v $PWD:/home/docker/$(basename $PWD) local/$image_tag bash
+        docker run --name $(basename $PWD) -it --rm -v $PWD:/home/docker/$(basename $PWD) local/$image_tag bash
     else
         docker run --name $(basename $PWD) --rm -v $PWD:/home/docker/$(basename $PWD) local/$image_tag $0 $*
     fi
